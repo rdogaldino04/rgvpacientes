907d3891bd0c24e42ae57a10f08c0ac4ab042212
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-08-10 19:55:42 -0300

    initial commit

---
+++ src/main/java/com/galdino/rgvpacientes/RgvPacientesApplication.java
@@ -0,0 +1,13 @@
+package com.galdino.rgvpacientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RgvPacientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RgvPacientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galdino/rgvpacientes/RgvPacientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galdino.rgvpacientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RgvPacientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
