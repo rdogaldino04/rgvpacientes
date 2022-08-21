28393cf1f363ff5abb6215d5f20c5de905b3b49e
DESKTOP-PHSPN30\rdogaldino0474 <raimundo.vasconcelos@mv.com.br>
2022-08-21 08:45:55 -0300

    feat: include deploy angular

---
+++ src/main/java/com/galdino/rgvpacientes/controller/ViewController.java
@@ -0,0 +1,13 @@
+package com.galdino.rgvpacientes.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "forward:/index.html";
+	}
+}
