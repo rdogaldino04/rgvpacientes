d4a67dd00d81cad50001ca225b1e86c75c7593ae
rdogaldino04 <rdogaldino04@gmail.com>
2023-09-18 09:29:17 -0300

    feat: name material already exists

--- src/main/java/com/galdino/rgvpacientes/repository/MaterialRepository.java
+++ src/main/java/com/galdino/rgvpacientes/repository/MaterialRepository.java
@@ -3,6 +3,12 @@ package com.galdino.rgvpacientes.repository;
 import com.galdino.rgvpacientes.model.Material;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MaterialRepository extends JpaRepository<Material, Long>, MaterialRepositoryQuery {
+
+    boolean existsByName(String name);
+
+    Optional<Material> findByName(String name);
 
 }
--- src/main/java/com/galdino/rgvpacientes/service/MaterialService.java
+++ src/main/java/com/galdino/rgvpacientes/service/MaterialService.java
@@ -5,19 +5,27 @@ import com.galdino.rgvpacientes.dto.material.MaterialDTO;
 import com.galdino.rgvpacientes.dto.material.MaterialInput;
 import com.galdino.rgvpacientes.model.Material;
 import com.galdino.rgvpacientes.repository.MaterialRepository;
+import com.galdino.rgvpacientes.service.exception.BusinessException;
 import com.galdino.rgvpacientes.service.exception.EntityInUseException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MaterialService {
 
+    public static final String THERE_IS_NO_MATERIAL_WITH_CODE = "There is no material with code %d";
     private final MaterialRepository materialRepository;
     private final MaterialMapper materialMapper;
     private final MovementItemService movementItemService;
+    @PersistenceContext
+    private EntityManager manager;
+
 
     public MaterialService(
             MaterialRepository materialRepository,
@@ -34,7 +42,7 @@ public class MaterialService {
 
     public MaterialDTO findById(Long id) {
         Material material = this.materialRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("There is no material with code %d", id)));
+                .orElseThrow(() -> new EntityNotFoundException(String.format(THERE_IS_NO_MATERIAL_WITH_CODE, id)));
         return MaterialDTO.builder()
                 .id(material.getId())
                 .name(material.getName())
@@ -45,6 +53,9 @@ public class MaterialService {
 
     @Transactional
     public MaterialDTO create(MaterialInput materialInput) {
+        if (materialRepository.existsByName(materialInput.getName())) {
+            throw new BusinessException(String.format("Material with the name %s already exists", materialInput.getName()));
+        }
         Material material = materialRepository.save(this.materialMapper.toEntity(materialInput));
         return this.materialMapper.toDTO(material);
     }
@@ -59,8 +70,15 @@ public class MaterialService {
                 .map(materialFound -> {
                     materialFound.setName(materialInput.getName());
                     materialFound.setExpirationDate(materialInput.getExpirationDate());
+
+                    manager.detach(materialFound);
+                    Optional<Material> existingMaterial = materialRepository.findByName(materialInput.getName());
+                    if (existingMaterial.isPresent() && !existingMaterial.get().equals(materialFound)) {
+                        throw new BusinessException(String.format("Material with the name %s already exists", materialInput.getName()));
+                    }
+
                     return materialMapper.toDTO(materialRepository.save(materialFound));
-                }).orElseThrow(() -> new EntityNotFoundException(String.format("There is no material with code %d", id)));
+                }).orElseThrow(() -> new EntityNotFoundException(String.format(THERE_IS_NO_MATERIAL_WITH_CODE, id)));
     }
 
     @Transactional
@@ -70,7 +88,7 @@ public class MaterialService {
         }
         materialRepository.delete(
                 materialRepository.findById(id)
-                        .orElseThrow(() -> new EntityNotFoundException(String.format("There is no material with code %d", id)))
+                        .orElseThrow(() -> new EntityNotFoundException(String.format(THERE_IS_NO_MATERIAL_WITH_CODE, id)))
         );
     }
 
