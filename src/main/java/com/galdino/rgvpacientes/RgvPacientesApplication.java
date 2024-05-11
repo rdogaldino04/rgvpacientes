6ff2806ef9635c02e3b37b23a97a2c44cfb16207
rdogaldino04 <rdogaldino04@gmail.com>
2024-05-11 09:32:31 -0300

    feat: add CustomJpaRepository

--- src/main/java/com/galdino/rgvpacientes/RgvPacientesApplication.java
+++ src/main/java/com/galdino/rgvpacientes/RgvPacientesApplication.java
@@ -1,9 +1,12 @@
 package com.galdino.rgvpacientes;
 
+import com.galdino.rgvpacientes.repository.CustomJpaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class RgvPacientesApplication {
 
 	public static void main(String[] args) {
