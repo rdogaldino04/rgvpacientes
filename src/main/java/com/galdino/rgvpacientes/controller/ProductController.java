10b74ae0b7fcf894c0ee4a24a5fcfaa8be8d13f3
rdogaldino04 <rdogaldino04@gmail.com>
2024-06-07 21:46:37 -0300

    add cache em produtos

--- src/main/java/com/galdino/rgvpacientes/controller/ProductController.java
+++ src/main/java/com/galdino/rgvpacientes/controller/ProductController.java
@@ -1,59 +1,56 @@
 package com.galdino.rgvpacientes.controller;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.galdino.rgvpacientes.dto.product.ProductDTO;
 import com.galdino.rgvpacientes.dto.product.ProductFilter;
 import com.galdino.rgvpacientes.dto.product.ProductInput;
-import com.galdino.rgvpacientes.util.page.PageWrapper;
+import com.galdino.rgvpacientes.mapper.ProductMapper;
+import com.galdino.rgvpacientes.model.Product;
 import com.galdino.rgvpacientes.service.ProductService;
+import com.galdino.rgvpacientes.util.page.PageWrapper;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @RestController
 @RequestMapping("api/products")
 public class ProductController {
 
     private final ProductService productService;
+    private final ProductMapper productMapper;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService, ProductMapper productMapper) {
         this.productService = productService;
+        this.productMapper = productMapper;
     }
 
     @GetMapping
     public PageWrapper<ProductDTO> getProductByFilter(@Valid ProductFilter productFilter,
-            @PageableDefault(size = 5) Pageable pageable) {
+                                                      @PageableDefault(size = 5) Pageable pageable) {
         return this.productService.getProductByFilter(productFilter, pageable);
     }
 
     @GetMapping("{id}")
     public ProductDTO findById(@PathVariable Long id) {
-        return this.productService.findById(id);
+        return productMapper.toDTO(productService.findById(id));
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public ProductDTO create(@RequestBody @Valid @NotNull ProductInput productInput) {
-        return this.productService.create(productInput);
+        return productMapper.toDTO(productService.create(productMapper.toEntity(productInput)));
     }
 
     @PutMapping("/{id}")
     public ProductDTO update(@PathVariable @NotNull @Positive Long id,
-            @RequestBody @Valid @NotNull ProductInput productInput) {
-        return productService.update(id, productInput);
+                             @RequestBody @Valid @NotNull ProductInput productInput) {
+        Product product = productMapper.toEntity(productInput);
+        product.setId(id);
+        return productMapper.toDTO(productService.update(product));
     }
 
     @DeleteMapping("/{id}")
